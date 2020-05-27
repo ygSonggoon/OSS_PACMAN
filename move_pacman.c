60d4f61e932af7f1102da398404c134b69191cc1
ygSonggoon <brutowa13@gmail.com>
2020-05-27 22:20:41 +0900

    [add, fix] - [스페인어 한국어로 번역 및 주석 작업]
    1. move_pacman.c
    팩맨의 움직임을 기능을 구현하는 함수입니다.
    'a', 's', 'w', 'd'으 방향키로 조정하며, 다음 위치가 벽이 아니고 방향키가 입력된 상태이면 움직임을 진행할 수 있습니다.
    
    2. move_eneies.c
    적들의 위치를 조정하는 기능을 합니다.
    적들은 한 칸씩 움직이거나 제자리에 있게 됩니다.
    게임이 시작된 상태라면 우선 적들의 존재 여부를 확인합니다.
    적이 존재하면 사전에 생성한 임의 값을 이용해서 X축으로 이동합니다.
    만약 생선된 임의 값이 0인 경우에는 실질적으로 X축 이동을 하지 않았으므않았으므로 Y축으로 이동을 하게 됩니다.
    ( X축 이동확률은 2/3, Y축 이동확률은 2/9, 제자리에 있을 확률은 1/9입니다.)

---
+++ move_pacman.c
@@ -0,0 +1,39 @@
+//팩맨 움직임.
+void move_pacman(Game* g)
+{
+    //switch문을 사용해서 더 간결히 정리하고 싶음.
+    //w키 조정시.
+    if(g->pacman.direction == UP_MOVE && !collision_wall_check(g, g->pacman.i-1, g->pacman.j))
+    {
+        g->pacman.i--;
+        //벽의 뚫린 부분으로 넘어감. 이하 내용 동일.
+C        if(g->pacman.i < 0)
+            g->pacman.i = CELL_SIZE-1;
+    }
+
+    //s키 조정시
+    else if(g->pacman.direction == DOWN_MOVE && !collision_wall_check(g, g->pacman.i+1, g->pacman.j))
+    {
+        g->pacman.i++;
+        if(g->pacman.i > CELL_SIZE-1)
+            g->pacman.i = 0;
+    }
+
+    //d키 조정시
+    else if(g->pacman.direction == RIGHT_MOVE && !collision_wall_check(g, g->pacman.i, g->pacman.j+1))
+    {
+        g->pacman.j++;
+        if(g->pacman.j > CELL_SIZE-1)
+            g->pacman.j = 0;
+    }
+
+    //a키 조정시
+    else if(g->pacman.direction == LEFT_MOVE && !collision_wall_check(g, g->pacman.i, g->pacman.j-1))
+    {
+        g->pacman.j--;
+        if(g->pacman.j < 0)
+            g->pacman.j = CELL_SIZE-1;
+    }
+}
+
+
