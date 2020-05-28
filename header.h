e6dd683c9314bc490419c69aa8a5742ba3d2ccd3
ygSonggoon <brutowa13@gmail.com>
2020-05-28 18:39:17 +0900

    [add] - [main에 있는 헤더를 묶은 모음집.]

---
+++ header.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "conio2.h"
+
+#define CELL_SIZE 13
+#define WALL 'X'
+#define BLANK ' '
+#define UP_MOVE 'w'
+#define DOWN_MOVE 's'
+#define LEFT_MOVE 'a'
+#define RIGHT_MOVE 'd'
+#define PAUSE 'p'
+#define MOVE_STATUS 'm'
+
