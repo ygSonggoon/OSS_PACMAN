9bd4feb25e3d1683e940df59755ac3a6c85928d7
ygSonggoon <brutowa13@gmail.com>
2020-06-02 23:44:54 +0900

    [fix] - cleancode화 진행(1)
    (가독성1) 사전에 약속했던 변수/함수 스타일명을 사용해서 코드 의미를 확실히 했습니다.
    (가독성2) 복잡한 조건문을 단순히 했습니다.
    (가독성3) 애매하고 복잡한 코드를 확실한 의미를 갖게 했습니다.
    
    [1] move_pacman
    (가독성 1)
    g->pacman.j++; 무의식적으로 누군가는 i가 X축 j가 Y축이라고 생각합니다.
    그러나 해당 코드에서는 i가 Y축을 의미했습니다. 그렇기에 X_axis, Y_axis등 구체적으로 작성했습니다.
    
    (가독성 2)
    똑같은 조건문을 매번 사용하며, 보기 안좋게 작성되어 있었습니다. 그렇기에 중복된 조건문은 한번만 사용했고,
    switch문을 사용해서 보기좋게 했습니다.
    
    [2] move_enemies
    (가독성 3)
    적은 (X, Y)좌표를 갖으므로 하나만 검사 혹은 동시에 검사하면 되는데 따로따로 검사를 하고 있었습니다.
    또한 X축 이동, Y축이동, 그리고 제자리에 있는 경우를 간단히 나타낼 수 있는데 매우 복잡하게 나타냈습니다.
    (원 작성자가 의도했던 확률을 분석해서 그대로 사용했습니다.)
    그렇기에 전체적으로 코드를 간단히 이해할 수 있도록 했습니다.

--- move_enemies.c
+++ move_enemies.c
@@ -1,76 +1,78 @@
-#include "header.h"
+/*
+ * move_enemies, 즉 적이 움직이는 기능을 하는 함수 입니다.
+ * 적은 X축으로 움직일 확률 2/3, Y축으로 움직일 확률 2/9, 제자리에 있을 확률 1/9를
+ * 랜덤값을 이용해서 해당 확률을 구분시켜 주었습니다.
+ **/
 
 void move_enemies(Game* g)
 {
 
     int rdm;
 
- C   //PAUSE인 경우는 2가지, 게임이 시작하지 않는 경우와, 플레이어와 부딪>힌 경우
+    //PAUSE인 경우는 2가지, 게임이 시작하지 않는 경우와, 플레이어와 부딪힌 경우
     if(g->enemies_movement != PAUSE)
     {
         for(int k=0; k < CELL_SIZE*CELL_SIZE; k++)
         {
-            //한칸씩 움직이게 설정
-            //x축과 y축 움직이는 설정내용인데, 구체적으로 실현했으면 좋겠>음.
 
-            //-1, 0, 1이 발생함.
-            rdm = (rand() % 3) - 1;
-
-
-
-            //적이 존재하는 x축 위에 있다면,
-            if(g->enemies[k].i >= 0)
+            //적이 존재한다면 적을 움직이게 합니다.
+            if(g->enemies[k].i >= 0 && g->enemies[k].j >= 0)
             {
-                //현재 위치에 + rdm(-1,0,1)값이 벽이 아니라면.
-                if(!collision_wall_check(g, g->enemies[k].i + rdm, g->enemies[k].j)){
-                    //해당 적의 x좌표 위치 변경.
-                    g->enemies[k].i += rdm;
+                 rdm = (rand() % 9);
 
-                    //다른 공간으로 넘어갈수 있게 설정.맵에서 뚫린 공간
-                    if(g->enemies[k].i < 0)
-                        g->enemies[k].i = CELL_SIZE-1;
-                    if(g->enemies[k].i > CELL_SIZE-1)
-                        g->enemies[k].i = 0;
-                }
-            }
+                //적을 오른쪽으로 움직이게 합니다.
+                 if(rdm <= 2)
+                 {
+                    if(!collision_wall_check(g, g->enemies[k].i+1, g->enemies[k].j))
+                    {
+                        g->enemies[k].i++;
 
-
-
-            //랜덤값이 0이어야, y축이 움직임.
-            if(rdm == 0)
-            {
-                //rdm은 -1,0,1이 발생이 가능함.
-		rdm = (rand()%3)-1;
-
-                //사실상 enemies[k].i가 존재 한다면 .j도 존재하므로, 해당 조건은 중복된 내용임.
-                if(g->enemies[k].j >= 0)
-                {
-                    //벽인지 확인하기.
-                    if(!collision_wall_check(g, g->enemies[k].i, g->enemies[k].j + rdm)){
-
-                        g->enemies[k].j += rdm;
-
-                        //구멍이 뚫린 곳을 통해 다른 곳으로 이동.
-                        if(g->enemies[k].j < 0)
-                            g->enemies[k].j = CELL_SIZE-1;
-                        if(g->enemies[k].j > CELL_SIZE-1)
-                            g->enemies[k].j = 0;
+                        if(g->enemies[k].i > CELL_SIZE - 1){
+                            g->enemies[k].i = 0;
+                        }
                     }
-                }
+                 }
+
+                //적을 왼쪽으로 움직이게 합니다.
+                 else if(rdm <= 5 && rdm > 2)
+                 {
+                    if(!collision_wall_check(g, g->enemies[k].i-1, g->enemies[k].j))
+                    {
+                        g->enemies[k].i--;
+
+                        if(g->enemies[k].i < 0){
+                            g->enemies[k].i = CELL_SIZE - 1;
+                        }
+                    }
+                 }
+
+                //적을 위쪽으로 움직이게 합니다.
+                 else if(rdm <= 6 && rdm > 5)
+                 {
+                    if(!collision_wall_check(g, g->enemies[k].i, g->enemies[k].j+1))
+                    {
+                        g->enemies[k].j++;
+
+                        if(g->enemies[k].j > CELL_SIZE - 1){
+                            g->enemies[k].j = 0;
+                        }
+                    }
+
+                 }
+
+                //적을 아래쪽으로 움직이게 합니다.
+                 else if(rdm <= 7 && rdm > 6)
+                 {
+                    if(!collision_wall_check(g, g->enemies[k].i, g->enemies[k].j-1))
+                    {
+                        g->enemies[k].j--;
+
+                        if(g->enemies[k].j < 0){
+                            g->enemies[k].j = CELL_SIZE - 1;
+                        }
+                    }
+                 }
             }
         }
     }
 }
-
-	
-
-
-
-
-
-
-
-
-
-
-
--- move_pacman.c
+++ move_pacman.c
@@ -1,41 +1,59 @@
-#include "header.h"
-
+/*
+팩맨이 움직이는 기능을 합니다. 사용자가 방향키를 입력한 경우 한칸 씩 움직이게 됩니다.
+*/
 void move_pacman(Game* g)
 {
 
-    //w키 조정시.
-    if(g->pacman.direction == UP_MOVE && !collision_wall_check(g, g->pacman.i-1, g->pacman.j))
+    // 동서남북 4가지 방향으로 움직일 수 있습니다.
+    switch(g->pacman.direction)
     {
-        g->pacman.i--;
-        //벽의 뚫린 부분으로 넘어감. 이하 내용 동일.
-        if(g->pacman.i < 0)
-            g->pacman.i = CELL_SIZE-1;
-    }
+        //북쪽 방향을 가리키는 경우 입니다.
+        case UP_MOVE :
 
-    //s키 조정시
-    else if(g->pacman.direction == DOWN_MOVE && !collision_wall_check(g, g->pacman.i+1, g->pacman.j))
-    {
-        g->pacman.i++;
-        if(g->pacman.i > CELL_SIZE-1)
-            g->pacman.i = 0;
-    }
+            //위쪽 방향이 벽인 경우 팩맨은 움직임을 진행하지 않습니다.
+            if(!collision_wall_check(g, g->pacman.y_axis - 1, g->pacman.x_axis)){
+                g->pacman.y_axis--;
+                //한칸 씩 움직이게 할 계획이므로 상태를 멈추게 합니다.
+                g->pacman.direction = PAUSE;
 
-    //d키 조정시
-    else if(g->pacman.direction == RIGHT_MOVE && !collision_wall_check(g, g->pacman.i, g->pacman.j+1))
-    {
-        g->pacman.j++;
-        if(g->pacman.j > CELL_SIZE-1)
-            g->pacman.j = 0;
-    }
+                //벽을 넘어간 경우 위치를 다시 설정해 줍니다.
+                if(g->pacman.y_axis < 0){
+                    g->pacman.y_axis = CELL_SIZE - 1;
+                }
+            }
+            break;
 
-    //a키 조정시
-    else if(g->pacman.direction == LEFT_MOVE && !collision_wall_check(g, g->pacman.i, g->pacman.j-1))
-    {
-        g->pacman.j--;
-        if(g->pacman.j < 0)
-            g->pacman.j = CELL_SIZE-1;
+        //위 내용과 동일한 과정을 지닙니다.
+        case DOWN_MOVE:
+            if(!collision_wall_check(g, g->pacman.y_axis + 1, g->pacman.x_axis)){
+                g->pacman.y_axis++;
+                g->pacman.direction = PAUSE;
+
+                if(g->pacman.y_axis > CELL_SIZE - 1){
+                    g->pacman.y_axis = 0;
+                }
+            }
+            break;
+
+        case RIGHT_MOVE:
+            if(!collision_wall_check(g, g->pacman.y_axis, g->pacman.x_axis + 1)){
+                g->pacman.x_axis++;
+                 g->pacman.direction = PAUSE;
+                if(g->pacman.x_axis > CELL_SIZE - 1){
+                    g->pacman.x_axis = 0;
+                }
+            }
+            break;
+
+        case LEFT_MOVE :
+            if(!collision_wall_check(g, g->pacman.y_axis, g->pacman.x_axis - 1)){
+                g->pacman.x_axis--;
+                g->pacman.direction = PAUSE;
+
+                if(g->pacman.x_axis < 0){
+                    g->pacman.x_axis = CELL_SIZE - 1;
+                }
+            }
+            break;
     }
 }
-
-
-
